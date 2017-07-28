aea90eb1797828d75fd9b144bd712b457ea695bb
Christopher Aubuchon <aubuchcl@gmail.com>
2017-07-28 16:30:04 -0700

    added database_setup.py and addd blank database restauantmenu.db

---
+++ vagrant/database_setup.py
@@ -0,0 +1,49 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+# in order to create our foreign key relationships / will also be used when we import our mapper
+from sqlalchemy.orm import relationship
+
+# used in the configuration code at the end of the file
+from sqlalchemy import create_engine
+
+# make an instance of the declarative_base class we just imported
+Base = declarative_base()
+
+
+# create classes that correspond to the tables we want to create in our database
+class Restaurant(Base):
+	# create a table representation
+	__tablename__ = 'restaurant'
+
+	name = Column(String(80), nullable = False)
+
+	id = Column(Integer, primary_key = True)
+
+class MenuItem(Base):
+	# create a table representation
+	__tablename__ = 'menu_item'
+
+	name = Column(String(80), nullable = False)
+
+	id = Column(Integer, primary_key = True)
+
+	course = Column(String(250))
+
+	description = Column(String(250))
+
+	prince = Column(String(8))
+
+	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+
+	restaurant = relationship(Restaurant)
+
+# end of file
+engine = create_engine(
+	'sqlite:///restaurantmenu.db')
+
+# go into the database and adds the classes we will soon create as new tables in our database
+Base.metadata.create_all(engine)
--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,33 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
 
+conn = psycopg2.connect("dbname=forum")
+
+cursor = conn.cursor()
+
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  posts_selector = "select * from posts;"
+  cursor.execute(posts_selector)
+  posts_results = cursor.fetchall()
+  conn.close()
+  return posts_results
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+  post_created = "insert into posts values(%s)", (content,);
+  cursor.execute(post_created)
+  conn.commit()
+  conn.close()
+  # POSTS.append((content, datetime.datetime.now()))
 
 
+#things I got wrong the first time
+## move connection variables into the methods so that it makes
+##sense to open and close the connection inside the method itself
+
+###make a DBNAME constant
